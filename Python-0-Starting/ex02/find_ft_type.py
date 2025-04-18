7d9004d067c3675a251ec0fa3bda4894182d3249
MEDBOUAYOUNE <bouayounemoha1@gmail.com>
2025-04-18 22:27:45 +0200

    Update DAY00|EX02

--- Python-0-Starting/ex02/find_ft_type.py
+++ Python-0-Starting/ex02/find_ft_type.py
@@ -2,10 +2,11 @@ from collections.abc import Collection
 
 def all_thing_is_obj(object: any) -> int:
     if isinstance(object, Collection):
+        type_obj = type(object)
         if isinstance(object, str):
-            print(f"{object}  is in the kitchen : {type(object)}")
+            print(f"{object}  is in the kitchen : {type_obj}")
         else:
-            print(f"{type(object).__name__} : {type(object)}")
+            print(f"{type_obj.__name__} : {type_obj}")
     else:
         print("Type not found")
     return 42
