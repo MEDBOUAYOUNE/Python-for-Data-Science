03ca22ef2d1a5bdfd9337185ab73d162bbd9ea6d
MEDBOUAYOUNE <bouayounemoha1@gmail.com>
2025-04-18 21:17:25 +0200

    Day00|EX00

---
+++ Python-0-Starting/ex00/Hello.py
@@ -0,0 +1,25 @@
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+
+
+#your code here
+
+ft_list[1] = "World!"
+
+ft_list_tuple    = list(ft_tuple)
+ft_list_tuple[1] = "Morocco!"
+ft_tuple         = tuple(ft_list_tuple)
+
+ft_set.remove("tutu!")
+ft_set.add("Ben Guerir!")
+
+ft_dict["Hello"] = "1337!"
+
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
