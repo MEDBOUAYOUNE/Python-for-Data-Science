84f1a112a2cbed45e4a25b07ec147ac3574e9a9b
Mohamed Bouayoune <mbouayou@c4r2p7.1337.ma>
2025-04-19 17:10:33 +0100

    DAY00|EX04

---
+++ Python-0-Starting/ex04/whatis.py
@@ -0,0 +1,16 @@
+import sys
+
+# print(len(sys.argv))
+try:
+    assert len(sys.argv) == 2, "AssertionError: argument is not an integer"
+    assert sys.argv[1].isdigit(), "AssertionError: argument is not an integer"
+
+    value = int(sys.argv[1])
+
+    if value % 2 == 0:
+        print("I'm Even.")
+    else:
+        print("I'm Odd.")
+
+except AssertionError as error:
+    print(error)
