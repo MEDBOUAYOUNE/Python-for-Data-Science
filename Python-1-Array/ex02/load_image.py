c3123f80b8d19b14b8fa19b72ad7a7d07f167aae
MEDBOUAYOUNE <bouayounemoha1@gmail.com>
2025-04-25 17:20:46 +0200

    DAY01|EX02

---
+++ Python-1-Array/ex02/load_image.py
@@ -0,0 +1,9 @@
+import numpy as np
+from numpy import ndarray
+from PIL import Image
+
+def ft_load(path: str) -> ndarray:
+    with Image.open(path) as image:
+        imageNp = np.asarray(image)
+        print(f"The shape of image is : {imageNp.shape}")
+        return imageNp
